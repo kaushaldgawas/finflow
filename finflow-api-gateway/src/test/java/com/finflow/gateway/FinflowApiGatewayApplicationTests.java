b85b66329bf11107d7098ebf68ee058de1acf315
kaushaldgawas <kaushal17gawas@gmail.com>
2026-06-29 16:50:04 +0530

    feat: add finflow-api-gateway with route configuration

---
+++ finflow-api-gateway/src/test/java/com/finflow/gateway/FinflowApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finflow.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinflowApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
